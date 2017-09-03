20613dc15b4e4942109c3b3881c7648cefaa47eb
justinthompson2112@icloud.com <justinthompson593@gmail.com>
2017-09-03 12:59:18 -0700

    commit

--- RemoteCommand/main.cpp
+++ RemoteCommand/main.cpp
@@ -62,12 +62,16 @@ void installRemoteCommand(){
 #endif
 	
 	// Have user set crontab
-	sprintf(toSystem, "clear && echo -e \"\n\n\n\n\nCalling crontab -e. This will open your crontab in the nano editor. The following line should be copied to your clipboard.\n\n* * * * * $HOME/RemoteCommand/RemoteCommand/RemoteCommand\n\nPaste the line into the editor (Command+v in OSX or Ctrl+Shift+V in *nix) to call RemoteCommand every minute, or edit it to suit your needs. When you're done, hit Ctrl+o to save and Ctrl+x to exit. To turn off mail notifications, add MAILTO=\"\" to the line above the one you're about to paste.\n\nHit any key to continue\"");
+	sprintf(toSystem, "clear && echo -e \"\n\n\n\n\nCalling crontab -e. The following line should be copied to your clipboard.\n\n* * * * * $HOME/RemoteCommand/RemoteCommand/RemoteCommand\n\nPaste the line into the editor (Command+v in OSX or Ctrl+Shift+V in *nix) to call RemoteCommand every minute, or edit it to suit your needs. When you're done, hit Ctrl+o to save and Ctrl+x to exit. To turn off mail notifications, add MAILTO=\\\"\\\" to the line above the one you're about to paste.\n\nHit any key to continue. This will open your crontab in the nano editor.\"");
 	system(toSystem);
 	
 	system("stty raw");
 	usrIn = getchar();
 	system("stty cooked");
+	
+	// Call crontab in nano
+	sprintf(toSystem, "env EDITOR=nano crontab -e");
+	system(toSystem);
 }
 
 int main(int argc, const char * argv[]) {
