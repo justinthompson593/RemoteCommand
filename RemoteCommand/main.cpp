dee735769e6408ef1faf4939a6e91072c73f75f2
justinthompson2112@icloud.com <justinthompson593@gmail.com>
2017-09-03 09:35:20 -0700

    this built

--- RemoteCommand/main.cpp
+++ RemoteCommand/main.cpp
@@ -11,24 +11,43 @@
 
 using namespace std;
 
+
 void installRemoteCommand(){
 	system("echo \"$(git config user.email)\" > gitEmail");
 	ifstream ifs("gitEmail");
 	string email((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
 	ifs.close();
+	
+	if( email.length() < 1 ){
+		cout << "ERROR: Cannot find git user email. Installation aborted." << endl;
+		return;
+	}
+	else{
+		system("rm gitEmail");
+	}
+	
 	string branchName = email.substr(0,email.find("@"));
 	
-	system("echo \"$HOME/RemoteCommand\" > installDir");
-	ifs.open("installDir");
-	string installDir((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
-	ifs.close();
-	installDir = installDir.substr(0,installDir.length()-1);
-	
-	cout << endl  << "Installing RemoteCommand for " << branchName << " in " << installDir << endl << endl;
-	
 	char toSystem[4096];
-	sprintf(toSystem, "cd $HOME && git clone -b master https://github.com/justinthompson593/RemoteCommand.git && cd RemoteCommand && git branch %s && git checkout %s && echo \"# %s\" > README.md && git add -A && git commit -m \"New branch: %s\" && git push -u origin %s", branchName.c_str(), branchName.c_str(), branchName.c_str(), branchName.c_str(), branchName.c_str());
+	
+	sprintf(toSystem, "clear && echo -e \"\n\n\n\n\nInstalling RemoteCommand\n\nThis will create a directory named RemoteCommand in $HOME\n\nHit any key to continue or q to quit\"");
 	system(toSystem);
+	
+	
+	system("stty raw");
+	char usrIn = getchar();
+	system("stty cooked");
+	
+	if( strncmp(&usrIn, "q", 1) == 0 || strncmp(&usrIn, "Q", 1) == 0 ){
+		cout << endl << endl << "Quitting." << endl;
+		return;
+	}
+	
+	
+	sprintf(toSystem, "cd $HOME && git clone -b master https://github.com/justinthompson593/RemoteCommand.git && cd RemoteCommand && git branch %s && git checkout %s && echo -e \"# %s\nCreated on $(date)\" > README.md && git add -A && git commit -m \"New branch: %s\" && git push -u origin %s", branchName.c_str(), branchName.c_str(), branchName.c_str(), branchName.c_str(), branchName.c_str());
+	system(toSystem);
+	
+	sprintf(toSystem, "cd $HOME/RemoteCommand/RemoteCommand && touch oldCommand");
 }
 
 int main(int argc, const char * argv[]) {
